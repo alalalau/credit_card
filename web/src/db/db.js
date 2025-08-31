17cd90dc2a8ba0a10454af7540c0fd2f4c6c4a2d
laualiza <alizalau@klick.com>
2025-08-30 23:20:07 -0400

    test

--- web/src/db/db.js
+++ web/src/db/db.js
@@ -5,4 +5,20 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
+// get transactions given month and year, get all days in the month
+export async function getTransactions(month, year) {
+  const { data, error } = await supabase
+    .from('transactions')
+    .select('*')
+    .eq('month', month)
+    .eq('year', year)
+    .order('date', { ascending: true })
+
+  if (error) {
+    console.error(error)
+    return []
+  }
+  return data
+}
+
 console.log('here',supabaseUrl, supabaseKey)
